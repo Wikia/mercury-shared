ccd0f91013c6dbf51a0f97c0931ddd122d471d91
Igor Rogatty <igor@wikia-inc.com>
2017-03-28 16:01:54 +0200

    Simple build

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+gulp.task('default', () =>
+	gulp.src('./src/*.js')
+		.pipe(concat('mercury-shared.js'))
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'))
+);
