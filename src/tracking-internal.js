181729ddec5933aff8d90875dcadb04714c71824
Igor Rogatty <igor@wikia-inc.com>
2017-10-11 12:59:26 +0200

    IRIS-4949 | stop calling tracking methods, leave it to apps

--- src/tracking-internal.js
+++ src/tracking-internal.js
@@ -118,7 +118,6 @@
 		console.info('Track pageView: Internal');
 	}
 
-
 	// API
 	M.tracker.Internal = {
 		track,
@@ -126,13 +125,4 @@
 		// those are needed for unit test
 		_createRequestURL: createRequestURL
 	};
-
-	if (M.getFromShoebox('runtimeConfig.noExternals') || M.getFromShoebox('serverError')) {
-		return;
-	}
-
-	M.tracker.Internal.trackPageView({
-		a: M.getFromShoebox('wikiPage.data.details.id'),
-		n: M.getFromShoebox('wikiPage.data.ns')
-	});
 })(window.M);
