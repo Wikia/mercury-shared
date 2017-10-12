815308b1080c764279990a60b617df260fb03fc9
Igor Rogatty <igor@wikia-inc.com>
2017-10-12 11:10:12 +0200

    IRIS-4949 | add missing globals

--- src/head/globals.js
+++ src/head/globals.js
@@ -1,9 +1,12 @@
-// initialize objects needed for MediaWiki based AbTesting
+// objects needed for Oasis
 window.Wikia = {};
+window.Mercury = {};
 
+// objects needed for AdEngine
 window.mw = {
 	loader: {
 		state: function () {
 		}
 	}
 };
+window.wgNow = new Date();
